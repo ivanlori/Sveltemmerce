64086f9b243ba8e4aa22a0c472474c98d207a659
Ivan Lori <ivan.lori@protonmail.com>
2021-08-26 10:49:31 +0200

    Chore: changes in configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import static_adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,7 +14,13 @@ const config = {
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: static_adapter()
+		// uncommnet for production
+		/* paths: {
+			base: '/sveltemmerce',
+			assets: '/sveltemmerce'
+		} */
 	}
 };
 
